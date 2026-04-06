080fa44da53a863065fbc0a6e36cb40b4ecd67ee
Aryan <aryanvj787@gmail.com>
2026-04-07 04:14:40 +0530

    database-connected-NeonDB-default-to-sqlite

--- src/backend/db/models.py
+++ src/backend/db/models.py
@@ -1,9 +1,12 @@
-from sqlalchemy import Column, Integer, String, Text, ForeignKey
+from datetime import datetime
+from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from .session import Base
 
 
 class Debate(Base):
+    """Table for debates"""
+
     __tablename__ = "debates"
 
     id = Column(Integer, primary_key=True, index=True)
@@ -14,6 +17,8 @@ class Debate(Base):
 
 
 class Agent(Base):
+    """Table for Agent with persona"""
+
     __tablename__ = "agents"
 
     id = Column(Integer, primary_key=True)
@@ -26,12 +31,18 @@ class Agent(Base):
 
 
 class Message(Base):
+    """Table for storing messages during rounds"""
+
     __tablename__ = "messages"
 
     id = Column(Integer, primary_key=True)
 
     debate_id = Column(Integer, ForeignKey("debates.id"))
     agent_name = Column(String)
-    content = Column(Text)
+    content = Column(Text)  # topic to discuss
+    internal_monologue = Column(Text, nullable=True)  # what agent does not want to say
+    withheld_info = Column(Text, nullable=True)  # info does not want to share
+    subtext = Column(Text, nullable=True)  # agents intent
+    timestamp = Column(DateTime, default=lambda: datetime.now())
 
     debate = relationship("Debate", back_populates="messages")
--- src/backend/db/session.py
+++ src/backend/db/session.py
@@ -1,9 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "sqlite:///./debate.db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Using NeonDB for storing data - free tier
+NEON_DB_URL = os.getenv("NEON_DB_CONNECTION_STRING")
+
+if NEON_DB_URL:
+    engine = create_engine(NEON_DB_URL, pool_pre_ping=True, pool_size=5)
+else:
+    DATABASE_URL = "sqlite:///./debate.db"
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
